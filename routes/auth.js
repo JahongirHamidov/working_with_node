adc555167170ed7d644895bf55ed9e7806e43550
JahongirHamidov <hamidovjahongir444@gmail.com>
2021-01-26 22:51:24 +0500

    login added

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const {register} = require('../controllers/auth')
+const {register, login} = require('../controllers/auth')
 
 const router = express.Router()
 
-router.post('/register', register)
+router.route('/register').post(register)
+router.route('/login').post(login)
 
 module.exports = router
