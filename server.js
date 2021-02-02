3f4978ca9fcc1d14af9360d5b0105e6c69959d65
JahongirHamidov <hamidovjahongir444@gmail.com>
2021-02-02 13:44:34 +0500

    add security packages

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const cookieParser = require('cookie-parser')
 const mongoSanitize = require('express-mongo-sanitize')
 const helmet = require('helmet')
 const xss = require('xss-clean')
+const hpp = require('hpp')
+const expressRateLimit = require('express-rate-limit')
+const cors = require('cors')
+
 const connectDB = require('./config/db')
 const errorHandler = require('./middleware/error')
 //Load env vars
@@ -47,6 +51,19 @@ app.use(helmet())
 
 // Prevent XSS attacks
 app.use(xss())
+
+// Rate limiting
+const limiter = expressRateLimit({
+    windowMs: 10 * 60 *1000, // 10 minutes,
+    max: 100
+})
+app.use(limiter)
+
+//Prevent http param pollution
+app.use(hpp())
+
+// Enable CORS
+app.use(cors())
 
 // Set static folder
 app.use(express.static(path.join(__dirname,'public')))
